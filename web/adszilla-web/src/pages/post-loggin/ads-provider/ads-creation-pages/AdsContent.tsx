1a27c575ebc9447e0f082cd651726b0fe379e868
Rasika Weragoda <rasika.weragodaz@gmail.com>
2022-03-03 01:18:08 +0530

    Update final page done

--- web/adszilla-web/src/pages/post-loggin/ads-provider/ads-creation-pages/AdsContent.tsx
+++ web/adszilla-web/src/pages/post-loggin/ads-provider/ads-creation-pages/AdsContent.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const AdsContent = () => {
   return (
-      <div className="dashboard-box margin-top-30 margin-bottom-30">
-        <div className="headline">
-          <h3><i className="icon-feather-folder-plus"></i> Files</h3>
+      <div className="dashboard-box margin-top-30 margin-bottom-30 row d-flex justify-content-center">
+        <div className="headline col-xl-12">
+          <h3><i className="icon-feather-folder-plus"></i>Upload Ads</h3>
+        </div>
+
+        <div className="content with-padding padding-bottom-10 col-xl-8">
+
+
+          <div className="row">
+
+
+
+          </div>
         </div>
       </div>
   );
